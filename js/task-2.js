47ab273d967799248390771e7384de875126c950
Lesia_Nazaruk <nazarullesia@gmail.com>
2024-09-24 11:33:51 +0300

    add all files

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    if (message.lenght <= maxLength) {
+    if (message.length <= maxLength) {
         return message;
     }
      return message.slice(0, maxLength) + "...";
